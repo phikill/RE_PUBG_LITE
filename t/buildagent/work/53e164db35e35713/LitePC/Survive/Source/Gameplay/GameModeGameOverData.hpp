24213f43a219ace8dc50b093109207f527974eaa
Gustavo Simões <72417648+phikill@users.noreply.github.com>
2025-08-11 19:15:41 -0300

    Delete t/buildagent/work/53e164db35e35713/LitePC directory

--- t/buildagent/work/53e164db35e35713/LitePC/Survive/Source/Gameplay/GameModeGameOverData.hpp
+++
@@ -1,9 +0,0 @@
-
-
-// ScriptStruct Gameplay.GameModeGameOverData
-// 0x0001
-struct FGameModeGameOverData
-{
-	unsigned char                                      UnknownData00[0x1];                                       // 0x0000(0x0001) MISSED OFFSET
-};
-
